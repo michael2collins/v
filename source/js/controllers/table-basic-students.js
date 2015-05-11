35015070da0234eaa67ae92093e578945bcd6cf7
michael2collins <michael.collins.natick@gmail.com>
2015-05-11 18:55:38 -0400

    test json working with uigrid

--- source/js/controllers/table-basic-students.js
+++ source/js/controllers/table-basic-students.js
@@ -1,23 +1,51 @@
-App.controller('StudentsTableBasicController3', ['$scope','$http','$routeParams', function($scope, $http, $routeParams){
+App.controller('StudentsTableBasicController', ['$scope','$http','$routeParams','uiGridConstants', function($scope, $http, $routeParams, uiGridConstants){
    $.fn.Data.Portlet();
 
       $scope.students = []; 
       
  //   setTimeout(function(){
+               $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
+            if( col.filters[0].term ){
+              return 'header-filtered';
+            } else {
+              return '';
+            }
+          };
+$scope.gridOptions = {
+    enableFiltering: true,
+         columnDefs: [
+      // default
+      { field: 'name', headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'ID',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'LastName' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'FirstName',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Email' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Email2',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Parent',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Phone' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'AltPhone',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Address',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'City',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'State',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'ZIP',headerCellClass: $scope.highlightFilteredHeader }
+    ]
 
-       $http.get('/testdata/students.json').
+};
+       $http.get('testdata/students.json').
          success(function(data, status, headers, config) {
-             console.log('got students');
+   //          console.log('got students');
              $scope.status = status;
-             $scope.students = data;
-             console.log(data);
+//             $scope.students = data;
+    $scope.gridOptions.data = data;
+   //          console.log(data);
          }).
          error(function (data, status, headers, config) {
                  //  Do some error handling here
             $scope.data = data || "Request Failed";
             $scope.status = status;
          });   
-  //  },50);
+
+         //  },50);
 
     /*
     setTimeout(function(){
@@ -43,19 +71,25 @@ App.controller('StudentsTableBasicController3', ['$scope','$http','$routeParams'
 */
 }]);
 
-App.controller('StudentsTableBasicController', ['StudentServices', '$scope', '$routeParams', '$log', function( StudentServices, $scope, $routeParams, $log){
+App.controller('StudentsTableBasicController2', ['StudentServices', 
+    '$scope', 
+    '$routeParams', 
+    '$log', 'uiGridConstants',
+    function( StudentServices, $scope, $routeParams, $log, uiGridConstants){
    $.fn.Data.Portlet();
-   $scope.students = []; 
+//   $scope.students = []; 
        
-//    var path = './testdata/students.json';
-    var path = '../v1/students';
+    var path = 'testdata/students_vsmall.json';
+//    var path = '../v1/students';
     $log.debug('Hello Debug!');
 
     
-    setTimeout(function(){
+ //   setTimeout(function(){
         // Init
-        var spinner = $( ".spinner" ).spinner();
-              
+  //      var spinner = $( ".spinner" ).spinner();
+
+        /* when i was controlling the list
+        
         $('#pre-selected-options').multiSelect();
         $('.selectpicker').selectpicker({
             iconBase: 'fa',
@@ -72,14 +106,44 @@ App.controller('StudentsTableBasicController', ['StudentServices', '$scope', '$r
             $scope.quantity = quantity;
             $log.debug('thenew' + quantity);
         };
-        
-        StudentServices.getAllStudents(path, function(data) {
-//            $scope.students = data.splice(0, 5);
-            $scope.students = data;
-    //        $scope.students = $scope.allStudents.splice(0,5);
+        */
+        /*
+          $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
+            if( col.filters[0].term ){
+              return 'header-filtered';
+            } else {
+              return '';
+            }
+          };
+*/
+$scope.gridOptions = {
+    enableFiltering: true
+    /*
+     columnDefs: [
+      // default
+      { field: 'name', headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'ID',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'LastName' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'FirstName',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Email' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Email2',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Parent',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Phone' ,headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'AltPhone',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'Address',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'City',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'State',headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'ZIP',headerCellClass: $scope.highlightFilteredHeader }
+    ]
+    */
+};
+    
+          StudentServices.getAllStudents(path, function(data) {
+            $scope.gridOptions.data = data;
+//            $scope.students = data;
         });
         
-    },50);
+//    },50);
     
 }]);
  
