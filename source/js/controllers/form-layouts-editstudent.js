c75e20cf145542b979093c0d35a26fb6d2c69db7
michael2collins <michael.collins.natick@gmail.com>
2015-05-31 20:05:20 -0400

    rount3 for edit student

--- source/js/controllers/form-layouts-editstudent.js
+++ source/js/controllers/form-layouts-editstudent.js
@@ -3,31 +3,57 @@
 
     angular
         .module('ng-admin')
-.controller('FormLayoutsControllerEditStudent', ['StudentServices', 
+.controller('FormLayoutsControllerEditStudent', FormLayoutsControllerEditStudent);
+
+    FormLayoutsControllerEditStudent.$inject = ['StudentServices', 
     '$scope', 
     '$routeParams', 
-    '$log',
-    function( StudentServices, $scope, $routeParams, $log){
+    '$log'
+    ];
+        
+    function FormLayoutsControllerEditStudent( StudentServices, $scope, $routeParams, $log){
+        /* jshint validthis: true */
+        var vm = this;
 
-        var menu_h = $('#sidebar').height();
-        $('#form-layouts-editstudent ul.nav-pills li a').live('click', function() {
-            var tab_id = $(this).attr('href');
-            var tab_h = $(tab_id).height();
-            if(tab_h < menu_h){
-                $(tab_id).css('height', '960px');
-            }
-        });
-        $.fn.Data.Portlet();
-        var path = '../v1/students/' + $routeParams.id;
-//        var path = '../v1/students/5340';
+        vm.getStudent = getStudent;
+        vm.students =[];
+        vm.menu_h = $('#sidebar').height();
+        vm.setHeight = setHeight;
+        vm.path = '../v1/students/' + $routeParams.id;
+//      vm.path = '../v1/students/5340';
+
         $log.debug('Hello Debug!');
-         $log.debug($routeParams.id);          
+        $log.debug($routeParams.id);          
 
-        StudentServices.getStudent(path, function(data) {
-            $log.debug('here too');
-            $log.debug(data);
-          
-            $scope.students = data;
-        });
-}]);
+        $.fn.Data.Portlet();
+        setHeight();
+        activate();
+        
+        function activate() {
+        return getStudent().then(function() {
+            $log.debug('activated EditStudent view');
+            });
+        }
+
+        
+        function getStudent() {
+            return StudentServices.getStudent(vm.path).then(function(data){
+                    $log.debug('getStudent returned data');
+                    $log.debug(data.data);
+                    vm.students = data.data;
+                    return vm.students;
+                });
+        }
+
+                 
+        function setHeight() {
+            $('#form-layouts-editstudent ul.nav-pills li a').live('click', function() {
+                var tab_id = $(this).attr('href');
+                var tab_h = $(tab_id).height();
+                if(tab_h < vm.menu_h){
+                    $(tab_id).css('height', '960px');
+                }
+            });
+        }
+    }
 })();    
