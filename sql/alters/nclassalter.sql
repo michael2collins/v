3164fa1d8479be6cc969a8cdbbe6d412b8116b55
michael2collins <michael.collins.natick@gmail.com>
2015-09-12 12:06:42 -0400

    switched to db driven studentclass

--- sql/alters/nclassalter.sql
+++ sql/alters/nclassalter.sql
@@ -21,3 +21,30 @@ UPDATE nclass SET classcat = 'special', pgmcat = 'other', agecat = 'adult' WHERE
 UPDATE nclass SET classcat = 'special', pgmcat = 'other', agecat = 'children or adult' WHERE nclass.class = 'Special Needs';
 UPDATE nclass SET classcat = 'wellness', pgmcat = 'other', agecat = 'adult' WHERE nclass.class = 'TaiChi';
 UPDATE nclass SET classcat = 'fitness', pgmcat = 'other', agecat = 'children or adult' WHERE nclass.class = 'Zoomba';
+
+UPDATE nclass SET pictureurl = 'adult.jpg' WHERE nclass.class = 'Adult';
+UPDATE nclass SET pictureurl = 'afterschool.jpg' WHERE nclass.class = 'After School';
+UPDATE nclass SET pictureurl = 'basicdragon.jpg' WHERE nclass.class = 'Basic Dragon';
+UPDATE nclass SET pictureurl = 'leopards.jpg' WHERE nclass.class = 'Basic Leopard';
+UPDATE nclass SET pictureurl = 'multiclass.jpg' WHERE nclass.class = 'BBT - Multiclasses';
+UPDATE nclass SET pictureurl = 'bbt1purple.jpg' WHERE nclass.class = 'BBT1 - Purple - B/G';
+UPDATE nclass SET pictureurl = 'bbt1leopard.jpg' WHERE nclass.class = 'BBT1 Leopard';
+UPDATE nclass SET pictureurl = 'bbt2.jpg' WHERE nclass.class = 'BBT2 - Green';
+UPDATE nclass SET pictureurl = 'bbt3.jpg' WHERE nclass.class = 'BBT3 - Brown';
+UPDATE nclass SET pictureurl = 'adultblackbelt.jpg' WHERE nclass.class = 'Blackbelt Adult';
+UPDATE nclass SET pictureurl = 'jrblackbelt.jpg' WHERE nclass.class = 'Blackbelt Jr';
+UPDATE nclass SET pictureurl = 'inactive.jpg' WHERE nclass.class = 'Inactive';
+UPDATE nclass SET pictureurl = 'injured.jpg' WHERE nclass.class = 'Injured';
+UPDATE nclass SET pictureurl = 'kickbox.jpg' WHERE nclass.class = 'Kickboxing';
+UPDATE nclass SET pictureurl = 'private.jpg' WHERE nclass.class = 'Privates Adult';
+UPDATE nclass SET pictureurl = 'privatechild.jpg' WHERE nclass.class = 'Privates Children';
+UPDATE nclass SET pictureurl = 'saturday.png' WHERE nclass.class = 'Saturday Only';
+UPDATE nclass SET pictureurl = 'selfdefence.jpg' WHERE nclass.class = 'Self Defense';
+UPDATE nclass SET pictureurl = 'specialneeds.jpg' WHERE nclass.class = 'Special Needs';
+UPDATE nclass SET pictureurl = 'taichi.jpg' WHERE nclass.class = 'TaiChi';
+UPDATE nclass SET pictureurl = 'zumba.jpg' WHERE nclass.class = 'Zoomba';
+
+
+
+
+
