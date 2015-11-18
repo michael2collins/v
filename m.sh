36981ac7ac834cd935577e7500a1f3a50a42e2e0
Michael Collins <michael.collins.natick@gmail.com>
2015-11-18 03:07:41 +0000

    linux enabling for koders

---
+++ m.sh
@@ -0,0 +1,5 @@
+cp -v ./source/form-layouts-editstudent.html ./app/templates/states
+cp -rv ./bower_components/jquery/* ./app/vendors/jquery
+cp -rv ./bower_components/lodash/* ./app/vendors/lodash
+cp -rv ./bower_components/angular-ui-mask/* ./app/vendors/angular-ui-mask
+cp -rv ./bower_components/angular-isotope/* ./app/vendors/angular-isotope
