21712eaed128ee7d2b9d2ab70866fcf4c4e302a4
Michael Collins <michael.collins.natick@gmail.com>
2018-12-31 20:47:18 +0000

    script updates

--- scripts/mailuser.sh
+++ scripts/mailuser.sh
@@ -5,6 +5,8 @@ $user = $1
 $site = $2
 $domain = $3
 $school = $4
+$nodeversion = $5
+
 cd /home/$user
 #su - marktest
 ln -s /home/michael2collins/Web/$site/v/scripts/mailforward.js mailforward.js
@@ -20,7 +22,7 @@ mkdir cur
 chmod 700 cur
 cd ..
 echo ":0" > .procmailrc
-echo "|/opt/nvm/versions/node/v9.3.0/bin/node /home/$user/mailforward.js" >> .procmailrc
+echo "|/opt/nvm/versions/node/$nodeversion/bin/node /home/$user/mailforward.js" >> .procmailrc
 echo '"|IFS=' '&&exec /usr/bin/procmail -f-||exit 75 #$user"' > .forward
 echo "host=$domain.villaris.us" > .env
 echo "school=$school" >> .env
