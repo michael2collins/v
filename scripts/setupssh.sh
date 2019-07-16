c7ae7c8f262f769342e32d1e168fdc0265229eea
Michael Collins <michael.collins.natick@gmail.com>
2019-07-16 22:06:15 +0000

    site ssl update

---
+++ scripts/setupssh.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# define settings here
+KEYSIZE=2048
+PASSPHRASE=waffle
+FILENAME=~/.ssh/id_test
+KEYTYPE=rsa
+HOST=45.79.153.63
+USER=michael2collins
+
+# use "-p <nr>" if the ssh-server is listening on a different port
+SSH_OPTS="-p 44444"
+
+#
+# NO MORE CONFIG SETTING BELOW THIS LINE
+#
+
+# check that we have all necessary parts
+SSH_KEYGEN=`which ssh-keygen`
+SSH=`which ssh`
+SSH_COPY_ID=`which ssh-copy-id`
+
+if [ -z "$SSH_KEYGEN" ];then
+    echo Could not find the 'ssh-keygen' executable
+    exit 1
+fi
+if [ -z "$SSH" ];then
+    echo Could not find the 'ssh' executable
+    exit 1
+fi
+
+# perform the actual work
+echo Creating a new key using $SSH-KEYGEN
+$SSH_KEYGEN -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
+RET=$?
+if [ $RET -ne 0 ];then
+    echo ssh-keygen failed: $RET
+    exit 1
+fi
+
+echo Adjust permissions of generated key-files locally
+chmod 0600 ${FILENAME}*
+RET=$?
+if [ $RET -ne 0 ];then
+    echo chmod failed: $RET
+    exit 1
+fi
+
+echo Copying the key to the remote machine $USER@$HOST
+if [ -z "$SSH_COPY_ID" ];then
+    echo Could not find the 'ssh-copy-id' executable, using manual copy instead
+    cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+else
+	$SSH_COPY_ID $SSH_OPTS -i $FILENAME.pub $USER@$HOST
+fi
+
+RET=$?
+if [ $RET -ne 0 ];then
+    echo ssh-copy-id failed: $RET
+    exit 1
+fi
+
+echo Adjusting permissions to avoid errors in ssh-daemon
+$SSH $SSH_OPTS $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
+RET=$?
+if [ $RET -ne 0 ];then
+    echo ssh-chmod failed: $RET
+    exit 1
+fi
+
+echo Setup finished, now try to run $SSH $SSH_OPTS -i $FILENAME $USER@$HOST
+
